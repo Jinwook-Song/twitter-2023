10c262352b1ede6ee9542f66cd61a8cad9eec193
jinwook song <wlsdnr129@naver.com>
2023-09-24 22:00:58 +0900

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './routes/home';
+import Profile from './routes/profile';
+import Login from './routes/login';
+import CreatAccount from './routes/create-account';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/create-account',
+    element: <CreatAccount />,
+  },
+]);
+
 function App() {
-  return <h1 className=''>Hello world!</h1>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
---
+++ src/routes/create-account.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CreatAccount() {
+  return <div>CreatAccount</div>;
+}
+
+export default CreatAccount;
