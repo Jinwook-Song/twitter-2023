94dbd3b86e0b5ff4e6263a874dffcc38cbc70320
jinwook song <wlsdnr129@naver.com>
2023-09-29 13:10:08 +0900

    realtime with snapshot

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,35 +1,41 @@
 import { useEffect, useState } from 'react';
 import { TweetModel } from '../models/tweet';
-import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 import { db } from '../firebase';
 import Tweet from './Tweet';
 
 function Timeline() {
   const [tweets, setTweets] = useState<TweetModel[]>([]);
 
-  const fetchTweets = async () => {
+  const fetchTweets = () => {
     const tweetQuery = query(
       collection(db, 'tweets'),
       orderBy('createdAt', 'desc')
     );
-    const snapshot = await getDocs(tweetQuery);
-    const tweets = snapshot.docs.map((doc) => {
-      const { uid, tweet, creator, photo, createdAt } =
-        doc.data() as TweetModel;
-      return {
-        id: doc.id,
-        uid,
-        tweet,
-        creator,
-        photo,
-        createdAt,
-      };
+    const unsubscribe = onSnapshot(tweetQuery, (snapshot) => {
+      const tweets = snapshot.docs.map((doc) => {
+        const { uid, tweet, creator, photo, createdAt } =
+          doc.data() as TweetModel;
+        return {
+          id: doc.id,
+          uid,
+          tweet,
+          creator,
+          photo,
+          createdAt,
+        };
+      });
+      setTweets(tweets);
     });
-    setTweets(tweets);
+
+    return unsubscribe;
   };
 
   useEffect(() => {
-    fetchTweets();
+    const unsubscribe = fetchTweets();
+    return () => {
+      unsubscribe && unsubscribe();
+    };
   }, []);
   return (
     <div className='flex flex-col gap-y-4'>
